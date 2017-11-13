3c470447046e9f26c7a49af8ed273158b91b625d
kruby3 <kruby3@gatech.edu>
2017-11-13 09:48:38 -0500

    Changed setup to include images

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ executables = [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="N-Back test",
     options={"build_exe": {"packages":["pygame"],
-                           "include_files":[]}},
+                           "include_files":["oneback.png","twoback.png", "threeback.png"]}},
     executables = executables
 )
